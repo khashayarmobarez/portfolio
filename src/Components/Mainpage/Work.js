d5fcec85980535cbf60af70caecbd7ecc1a2609e
khashayar <khashayarmobarezbusiness@gmail.com>
2025-05-20 02:15:02 +0330

    Implement code changes to enhance functionality and improve performance

--- src/Components/Mainpage/Work.js
+++ src/Components/Mainpage/Work.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import styles from './Work.module.css'
 import stylesFloat from '../FloatStyles/FloatLeft.module.css'
 
-import docWebsite from '../../media/Pics/drbabakzandi-website.png'
-import blogPage from '../../media/Pics/Web-capture-mobarez-blog.jpeg'
+import digilogbook from '../../media/Pics/digilogbook.png'
+import mobarrez from '../../media/Pics/mobarrez.png'
 import ProjectsCards from '../Reusable components/ProjectsCards';
 
 const Work = () => {
@@ -50,20 +50,20 @@ const Work = () => {
 
                     {/* texts container Dr.zandi*/}
                     <div id='descriptions-container' className=' z-10 w-[16rem] h-[320px] flex flex-col items-center justify-around -mt-[20rem] md:mr-12 md:mt-[-21rem] md:w-1/3 md:items-start md:justify-center'>
-                        <a href='https://khashayarmobarez.github.io/mobarez-blog-page/' target="_blank" rel="noopener noreferrer" className=' text-[24px] font-bold hover:underline underline-offset-4 md:pl-5'>Mobarez blog project</a>
-                        <p id='' className='text-center text-lg md:bg-[#112240] md:p-2 md:h-auto md:w-[140%] md:text-start rounded-lg md:-mr-[10%]'>Mobarez Blog, meticulously crafted with
-                        <span className=' font-semibold text-lg text-[#64FFDA]' > GraphQL</span> and <span className=' font-semibold text-lg text-[#64FFDA]' >material UI Technology, </span>seamlessly blends a captivating user experience with
+                        <a href='https://mobarrez.com' target="_blank" rel="noopener noreferrer" className=' text-[24px] font-bold hover:underline underline-offset-4 md:pl-5'>Founder of Mobarrez Co</a>
+                        <p id='' className='text-center text-lg md:bg-[#112240] md:p-2 md:h-auto md:w-[140%] md:text-start rounded-lg md:-mr-[10%]'>Mobarrez Company website crafted with
+                        <span className=' font-semibold text-lg text-[#64FFDA]' >NextJs</span> and <span className=' font-semibold text-lg text-[#64FFDA]' >daisy ui, </span>seamlessly blends a captivating user experience with
                         <span className=' font-semibold text-lg text-[#64FFDA]' > efficient data fetching,</span> embodying a sophisticated fusion of    
                         <span className=' font-semibold text-lg text-[#64FFDA]' > modern web development technologies</span> for an immersive blogging platform</p>
                         <div className='flex w-full justify-between font-semibold md:self-end md:w-[90%] md:flex-col lg:flex-row'>
-                            <p className=' text-[16px]' >react</p>
+                            <p className=' text-[16px]' >NextJs</p>
                             <p className=' text-[16px]' >Graphql</p>
                             <p className=' text-[16px]' >Material UI</p>
                         </div>
                     </div>
 
                     {/* image mobarez blog*/}
-                    <img alt='weblog website' src={blogPage} className=' z-0 h-[345px] w-[300px] blur-[6px] brightness-[20%] mt-[-21rem] grayscale-[50%] rounded md:w-[500px] md:h-[300px] md:blur-0 md:brightness-90 md:grayscale-0 hover:border-solid hover:border-4 hover:border-slate-50' />
+                    <img alt='weblog website' src={mobarrez} className=' z-0 h-[345px] w-[300px] blur-[6px] brightness-[20%] mt-[-21rem] grayscale-[50%] rounded md:w-[500px] md:h-[300px] md:blur-0 md:brightness-90 md:grayscale-0 hover:border-solid hover:border-4 hover:border-slate-50' />
 
                 </div>
 
@@ -72,20 +72,20 @@ const Work = () => {
 
                     {/* image Dr.zandi*/}
                     <a href='https://khashayarmobarez.github.io/dentalOfficeProject/' target="_blank" rel="noopener noreferrer">
-                        <img alt='doc website' src={docWebsite} className=' z-0 h-[345px] w-[300px] blur-[6px] brightness-[20%] grayscale-[50%] rounded md:w-[500px] md:h-[400px] md:blur-0 md:brightness-90 md:grayscale-0 hover:border-solid hover:border-4 hover:border-slate-50' />
+                        <img alt='doc website' src={digilogbook} className=' z-0 h-[345px] w-[300px] blur-[6px] brightness-[20%] grayscale-[50%] rounded md:w-[500px] md:h-[400px] md:blur-0 md:brightness-90 md:grayscale-0 hover:border-solid hover:border-4 hover:border-slate-50' />
                     </a>
 
                     {/* texts container Dr.zandi*/}
                     <div id='descriptions-container' className=' z-10 w-[16rem] h-[320px] flex flex-col items-center justify-around -mt-[21rem] md:mt-0 md:w-1/3 md:items-end md:justify-center'>
-                        <a href='https://khashayarmobarez.github.io/dentalOfficeProject/' target="_blank" rel="noopener noreferrer" className=' text-2xl font-bold hover:underline underline-offset-4'>Dr.Zandi Website</a>
-                        <p id='' className='text-center text-lg md:bg-[#112240] md:p-2 md:h-auto md:w-[140%] md:text-start rounded-lg md:-mr-[10%]'>Designed and developed a user-friendly website for a doctor, enabling patients to easily 
-                        <span className=' font-semibold text-lg text-[#64FFDA]' > schedule appointments</span>. Incorporated a library of 
-                        <span className=' font-semibold text-lg text-[#64FFDA]' > educational videos</span> and 
-                        <span className=' font-semibold text-lg text-[#64FFDA]' > showcased</span> the doctor's professional expertise and services.</p>
+                        <a href='https://digilogbook-landing.vercel.app/' target="_blank" rel="noopener noreferrer" className=' text-2xl font-bold hover:underline underline-offset-4 ml-2 text-center'>Digilogbook lead Front-end engineer</a>
+                        <p id='' className='text-center text-lg md:bg-[#112240] md:p-2 md:h-auto md:w-[140%] md:text-start rounded-lg md:-mr-[10%]'>developed a user-friendly website for parbaz company as the lead front-end engineer for 1 and a half years, enabling pilots to easily 
+                        <span className=' font-semibold text-lg text-[#64FFDA]' > log their flight data</span>. Incorporated a full application
+                        <span className=' font-semibold text-lg text-[#64FFDA]' > flight record system</span> and 
+                        <span className=' font-semibold text-lg text-[#64FFDA]' > showcased</span> the pilots flight records.</p>
                         <div className='flex w-full justify-between font-semibold md:self-end md:w-[90%] md:flex-col lg:flex-row'>
                             <p className=' text-sm' >react</p>
-                            <p className=' text-sm' >styled components</p>
-                            <p className=' text-sm' >REST API</p>
+                            <p className=' text-sm' >Nextjs</p>
+                            <p className=' text-sm' >REST APIs</p>
                         </div>
                     </div>
                 </div>
